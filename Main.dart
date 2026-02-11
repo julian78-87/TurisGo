212c56bcca0912fa57f7425acbd2490d48ad6b8b
cristecena <cristianandres210712@gmail.com>
2026-02-11 07:20:37 -0500

    Add files via upload

---
+++ Main.dart
@@ -0,0 +1,23 @@
+import 'package:evv/Pantallas/Ingreso.dart';
+import 'package:evv/Pantallas/Principal.dart';
+import 'package:evv/Pantallas/Registro.dart';
+import 'package:evv/Pantallas/ingreso_servicios_turisticos.dart';
+import 'package:flutter/material.dart';
+
+class Conexion extends StatelessWidget {
+  const Conexion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Principal(),
+
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/create_accommodation': (context) => const HomePage(),
+      },
+    );
+  }
+}
