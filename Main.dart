996941dfdbd854321d6ed3fc78353191467865bb
Julian78 <julianandresmaldonado78@gmail.com>
2026-02-12 07:13:57 -0500

    filtros en 2 archivos fornt y back

--- Main.dart
+++ Main.dart
@@ -4,7 +4,7 @@ import 'package:evv/Pantallas/Registro.dart';
 import 'package:evv/Pantallas/ingreso_servicios_turisticos.dart';
 import 'package:flutter/material.dart';
 
-class Conexion extends StatelessWidget {
+class Conexion extends StatelessWidge {
   const Conexion({super.key});
 
   @override
